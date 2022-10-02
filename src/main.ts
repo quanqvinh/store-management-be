5e51d76c8f951260075e8d9131f85385233c6bfe
quanqvinh <nguyenquanqvinh@gmail.com>
2022-10-02 15:31:41 +0700

    Add Helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/modules/app/app.module'
 import { ConfigService, ConfigModule } from '@nestjs/config'
+import helmet from 'helmet'
 
 async function bootstrap() {
 	await ConfigModule.envVariablesLoaded
 	const app = await NestFactory.create(AppModule)
+
+	// Security middlewares
+	app.use(helmet())
+
 	const configService = app.get(ConfigService)
 	const port = configService.get<number>('port') || 8080
 	await app.listen(port, () => {
